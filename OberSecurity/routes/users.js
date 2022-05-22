ffae8aceb648ca1d834db97bad7b2fce8e79c67a
JHKkmu <jhk8715@kmu.kr>
2022-05-22 13:50:51 +0900

    OH!

--- OberSecurity/routes/users.js
+++ OberSecurity/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const models = require("../models");
+const crypto = require('crypto');
 const router = express.Router();
 
 // users(.js)/sign_up 접속시 get.
@@ -9,12 +10,20 @@ router.get('/sign_up', function(req, res, next) {
 
 // users(.js)/sign_up 접속시 POST 출력값.
 router.post("/sign_up", function(req,res,next){
+  console.log("DB 되냐");
   let body = req.body;
+
+  let inputPassword = body.password;
+  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
+  let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+  console.log("DB 되냐");
 
   models.user.create({
     name: body.userName,
     email: body.userEmail,
-    password: body.password
+    password: body.password,
+    salt: salt
   })
   .then( result => {
     console.log("성공");
@@ -25,30 +34,39 @@ router.post("/sign_up", function(req,res,next){
     console.log(err)
   })
 })
-
-// users(.js)/login 접속시 get.
-router.get('/login', function(req, res, next) {
-  res.render("user/Login");
+// 메인 페이지
+router.get('/', function(req, res, next) {
+  res.send('환영합니다~');
 });
 
-// users(.js)/login 접속시 POST 출력값.
-router.post("/login", function(req,res,next){
+// 로그인 GET
+router.get('/login', function(req, res, next) {
+  res.render("users/login");
+});
+
+// 로그인 POST
+router.post("/login", async function(req,res,next){
   let body = req.body;
 
-  models.user.create({
-    name: body.userName,
-    email: body.userEmail,
-    password: body.password
-  })
-  .then( result => {
-    console.log("성공");
-    res.redirect("/users/sign_up");
-  })
-  .catch( err => {
-    console.log("실패");
-    console.log(err)
-  })
-})
+  let result = await models.user.findOne({
+      where: {
+          email : body.userEmail
+      }
+  });
 
+  let dbPassword = result.dataValues.password;
+  let inputPassword = body.password;
+  let salt = result.dataValues.salt;
+  let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+  if(dbPassword === hashPassword){
+      console.log("비밀번호 일치");
+      res.redirect("/user");
+  }
+  else{
+      console.log("비밀번호 불일치");
+      res.redirect("/user/login");
+  }
+});
 
 module.exports = router;
