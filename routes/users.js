422ae24541a0a9b5cb03deadfaa8577be0e4fbdb
JHKkmu <jhk8715@kmu.kr>
2022-05-28 00:04:49 +0900

    자동 로그인 마무리

--- routes/users.js
+++ routes/users.js
@@ -33,20 +33,57 @@ const generateRefreshToken = (id) => {
   })
 }
 
-// Access Token 유효성 검사
-const f_authenticateAccessToken = (req, res) =>{
+// 1 . ACCESS 만료 REFRESH 만료 => users/login
+// 2 . ACCESS 만료 REFRESH 유효 => Access 재발급 => users
+// 3 . ACCESS 유효 REFRESH 유효 => Refresh 재발급 => users
+// 4 . ACCESS 유효 REFRESH 유효 => users
+// 자동로그인 Access Token 유효성 검사
+const f_authenticateToken = (req, res) =>{
+  let id = req.body.userEmail;
   let Access = req.cookies.OberUser_Access;
+  let Refresh = req.cookies.OberUser_Refresh;
 
+  // Access 토큰이 없다면,
   if(!Access){
     console.log("Access 토큰이 없습니다.");
-    res.redirect("/users/login");
-  }
 
-  let decode = jwt.verify(Access, process.env.ACCESS_TOKEN_SECRET);
+    // 토큰 둘 다 없는 경우,
+    if(!Refresh){
+      console.log("Access 및 Refresh 토큰이 없습니다.");
+      res.redirect("/users/login");
+    }
+    // Access 토큰은 없지만, Refresh는 있는 경우.
+    else{                     
+      console.log("Access 토큰은 없지만, Refresh 토큰은 존재합니다.");
+      console.log("Access 토큰을 재발급 합니다.");
 
-  if(decode){
-    console.log("Access 토큰이 존재합니다.");
-    res.redirect("/users");
+      let AccessToken = generateAccessToken(id);
+
+      // AccessToken 쿠키 저장
+      res.cookie("OberUser_Access", AccessToken , {
+        expires: new Date(Date.now() + 900000),
+        httpOnly: true      // XSS 공격 대처방안.
+      });
+      // 하나라도 토큰이 없을때 발급 완료
+      // 토큰이 전부 재발급되었으니, 인증 및 로그인 절차
+      console.log("Access 토큰이 존재합니다.");
+      res.redirect("/users");
+    }
+  }// Refresh 토큰이 없다면.
+  else if(!Refresh){
+
+    // Access 토큰은 있지만, Refresh 토큰이 없는 경우
+    console.log("Refresh 토큰은 없지만, Access 토큰은 존재합니다.");
+    console.log("Refresh 토큰을 재발급 합니다.");
+
+    let RefreshToken = generateRefreshToken(id);
+
+    // RefreshToken 쿠키 저장
+    res.cookie("OberUser_Refresh", RefreshToken , {
+      expires: new Date(Date.now() + 900000),
+      httpOnly: true      // XSS 공격 대처방안.
+    });
+    
   }
 };
 
@@ -54,19 +91,13 @@ const f_authenticateAccessToken = (req, res) =>{
 const authenticateAccessToken = (req, res) =>{
   let Access = req.cookies.OberUser_Access;
 
-  if(!Access){
-    console.log("Access 토큰이 없습니다.");
-  }
-
+  // 하나라도 토큰이 없을때 발급 완료
+  // 토큰이 전부 재발급되었으니, 인증 및 로그인 절차
   let decode = jwt.verify(Access, process.env.ACCESS_TOKEN_SECRET);
 
-  if(!decode){
-    console.log("Access 토큰이 없습니다.");
-    return 0;
-  }
-  else{
+  if(decode){
     console.log("Access 토큰이 존재합니다.");
-    return 1;
+    res.redirect("/users");
   }
 };
 
@@ -119,9 +150,9 @@ router.get('/f_login', function(req, res, next) {
 
 // 로그인 JWT 테스트 페이지 POST
 router.post('/f_login', function(req, res, next) {
-  f_authenticateAccessToken(req, res);
+  f_authenticateToken(req, res);
+  authenticateAccessToken(req, res);
 });
-
 
 //###############################################################################
 // 로그인 GET
