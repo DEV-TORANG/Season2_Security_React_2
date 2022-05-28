b5f602c723663ee9a828b6c526aaca3ca0bb79ed
JHKkmu <jhk8715@kmu.kr>
2022-05-28 21:57:29 +0900

    Redux 설치 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Register from './routes/Register';
+
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/" element={Home} />
+            <Route exact path="/sign_in" element={Login} />
+            <Route exact path="/sign_up" element={Register} />
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
