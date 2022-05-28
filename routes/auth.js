28584e2bad0bdbc2486fc3a1aefd854585c74450
JHKkmu <jhk8715@kmu.kr>
2022-05-28 15:41:27 +0900

    React를 위한 삭제

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require("./users");
-
-const user = require('../routes/users');
-const {authenticate} = require('../routes/jwt');
-
-router.route('/user').get( authenticate, user.userLogin);
-
-module.exports = router
