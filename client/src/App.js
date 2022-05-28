0ab157b189d92cd53b78a28bed4f7ef61d66afa1
JHKkmu <jhk8715@kmu.kr>
2022-05-28 21:42:35 +0900

    스켈레톤 재생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,8 @@
-import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Navbar from './components/Navbar';
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
-
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/sign_in" component={LoginPage} />
-          <Route exact path="/sign_up" component={RegisterPage} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      Hello
+    </div>
   );
 }
 
--- client/src/routes/Login.js
+++
@@ -1,9 +0,0 @@
-function Login() {
-    return (
-      <div>
-        <h2>Home</h2>
-      </div>
-    );
-  }
-
-export default Login;
