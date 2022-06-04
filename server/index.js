af65d0f481a36cbb13748208355ed81422d468fc
JHKkmu <jhk8715@kmu.kr>
2022-06-04 13:24:49 +0900

    테스트

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express")
+const mongoose = require('mongoose')
+const config = require('./config/dev')
 const app = express()
 const port = 5000
+
+mongoose.connect(config.mongoURI)
+.then(()=>console.log(' == MongoDB Connected == '))
+.catch(err=>console.log(err))
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
